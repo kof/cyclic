9a6ad146b60f82a29e5a91e41f4a76afa3d94cec
Oleg Slobodskoi <oleg008@gmail.com>
2014-12-27 13:55:11 +0100

    description

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict'
 
+/**
+ * Cyclic is an api for creating single or bidirectional bindings between any kind of objects.
+ *
+ * @copyright Oleg Slobodskoi 2014
+ * @website https://github.com/kof/cyclic
+ * @license MIT
+ */
+
 module.exports = require('./lib')
